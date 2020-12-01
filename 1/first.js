5575f8d6d1ebf0a44b2f3ce518251deb573c6e32
Samuel Attard <samuel.r.attard@gmail.com>
2020-11-30 21:38:53 -0800

    Day 1

--- 1/first.js
+++ 1/first.js
@@ -1,3 +1,13 @@
-const input = global.loadInput().lines();
+const input = global
+  .loadInput()
+  .lines()
+  .map((i) => parseInt(i, 10));
 
-// TODO:
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j < input.length; j++) {
+    if (i === j) continue;
+
+    const sum = input[i] + input[j];
+    if (sum === 2020) console.log(input[i] * input[jk]);
+  }
+}
--- 1/second.js
+++ 1/second.js
@@ -1,3 +1,13 @@
-const input = global.loadInput().lines();
+const input = global
+  .loadInput()
+  .lines()
+  .map((i) => parseInt(i, 10));
 
-// TODO
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j < i; j++) {
+    for (let k = 0; k < j; k++) {
+      const sum = input[i] + input[j] + input[k];
+      if (sum === 2020) console.log(input[i] * input[j] * input[k]);
+    }
+  }
+}
